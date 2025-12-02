746979794a35643185bc8462d67711af7df4ad74
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-12-02 16:56:59 +0100

    stautsclean for push heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
+  SYSTEM_PROMPT = "Tu es un la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical. Tu dois obtenir la réponse a 2 questions: La personne est elle consciente? La personne respire t'elle? Si la personne est inconsciente et en apnée tu évoque clairement que nous sommes en situation d'arret cardiaque. Tu dois rassurer l'utilisateur"
   def create
+
+def create
+  # @chat = current_user.chats.find(params[:chat_id])
+  # raise
+  # @challenge = @chat.challenge
+
+  # @message = Message.new(message_params)
+  # @message.chat = @chat
+  # @message.role = "user"
+
+  # if @message.save
+  #   # ...
+  # end
+end
   end
 end
