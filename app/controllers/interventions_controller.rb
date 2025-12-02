f14778b916ded3d6e87d9c3c2a1452066a0a9c41
Julien Lemaire <julien_lemaire_62@yahoo.fr>
2025-12-02 14:30:30 +0100

    show inter

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,15 +1,23 @@
 class InterventionsController < ApplicationController
-
-  def create
+  def index
+    @interventions = Intervention.all
   end
 
   def show
+    @intervention = Intervention.find(params[:id])
+  end
+
+  def create
+    @intervention = Intervention.new(intervention_params)
+    @intervention.save
   end
 
   def recap
   end
 
-  def index
-  end
+private
 
+  def intervention_params
+    params.require(:intervention).permit(:address, :title, :age, :start_time, :end_time)
+  end
 end
