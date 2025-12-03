7839cc642d9e8e04506c32bde21eb520b378d5d6
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-12-03 10:23:46 +0100

    merge chat ia primitif

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  SYSTEM_PROMPT =  "Oublies la politesse. Tu es un la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical. Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle? Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes. Tu dois faire des phrases concises, tu es calme. UNIQUEMENT Si tu as obtenue la réponse a 1 et 2 et que le user te demande quoi faire dit lui de cliquer sur le bouton et qu'un guide va suivre Tu DOIS connaitre la réponse a 1 et 2 avant de lui dire de cliquer. Tu DOIS dire de cliquer pour continuer UNIQUEMENT si la réponse a 1 ET 2 est negative"
+  SYSTEM_PROMPT =  "Oublies la politesse. Tu es un la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical. Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle? Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes. Tu dois faire des phrases concises, tu es calme. UNIQUEMENT Si tu as obtenue la réponse a 1 et 2 et que l'utilisateur te demande quoi faire repond de cliquer sur le bouton et qu'un guide va suivre Tu DOIS connaitre la réponse a 1 et 2 avant de lui dire de cliquer. Tu DOIS dire de cliquer pour continuer UNIQUEMENT si la réponse a 1 ET 2 est negative"
 
 
   def create
@@ -34,12 +34,12 @@ class MessagesController < ApplicationController
   end
 
 def emmergency_case
-  @choice = [] # Le but est que le chat puisse choisir la bonne urgence, PEUT faire un hash dans lequel on stock l'ID de l'urgence et plus tard on s'en sert pour afficher la show de la bonne case.
+  @choice = [] # Le but est que le chat puisse choisir la bonne urgence, PEUT ETRE faire un hash dans lequel on stock l'ID de l'urgence et plus tard on s'en sert pour afficher la show de la bonne case.
   @cases.each do |emergency|
     @choice << emergency.name
   end
   @choice = @choice.join(" ")
-  "Ton but est de determiner de quelle situation parmis:#{@choice} et uniquement parmis #{@choice} Tu dois enoncer clairement dans quel nous #{@choice} sommes"
+  "Ton but est de determiner de quelle situation parmis:#{@choice} et uniquement parmis #{@choice} Tu dois enoncer clairement dans quelle #{@choice} nous sommes"
 end
 
 end
