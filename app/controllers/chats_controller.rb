64d5658f3136e07b799d4c3ffb00897b4746eb50
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-12-03 16:01:57 +0100

    ChatIA inconstant, echec fullIA, essais avec logique implementé en backend

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,10 @@
 class ChatsController < ApplicationController
-    SYSTEM_PROMPT =  "Oublies la politesse. Tu es un la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical. Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle? Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes. Si tu n'as pas les réponses a la question si la personne est consciente ET la réponse a la question si la personne respire tu as INTERDICTION de passer à la suite, tu dois redemander à l'utilisateur ces informations jusqu'a ce qu'il te réponde 'oui' ou 'non au minimus aux deux questions. Tu dois faire des phrases concises, tu es calme. UNIQUEMENT Si tu as obtenue la réponse a 1 et 2 et que l'utilisateur te demande quoi faire repond de cliquer sur le bouton et qu'un guide va suivre Tu DOIS connaitre la réponse a 1 et 2 avant de lui dire de cliquer. Tu DOIS dire de cliquer pour continuer UNIQUEMENT si la réponse a 1 ET 2 est negative"
+    SYSTEM_PROMPT =  "Oublies la politesse. L'utilisateur est témoin d'un problème médical sur une victime.
+  Task: Ta tâche est de definir dans quelle situation est la victime.
+  Step by step: Tu dois en premier obtenir la réponse a la question:
+        1 = La personne est elle consciente?"
+
+    # Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes. Si tu n'as pas les réponses a la question si la personne est consciente ET la réponse a la question si la personne respire tu as INTERDICTION de passer à la suite, tu dois redemander à l'utilisateur ces informations jusqu'a ce qu'il te réponde 'oui' ou 'non au minimus aux deux questions. Tu dois faire des phrases concises, tu es calme. UNIQUEMENT Si tu as obtenue la réponse a 1 et 2 et que l'utilisateur te demande quoi faire repond de cliquer sur le bouton et qu'un guide va suivre Tu DOIS connaitre la réponse a 1 et 2 avant de lui dire de cliquer. Tu DOIS dire de cliquer pour continuer UNIQUEMENT si la réponse a 1 ET 2 est negative"
 
   def create
     @intervention = Intervention.new(user: current_user)
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,20 @@
 class MessagesController < ApplicationController
-  SYSTEM_PROMPT =  "Oublies la politesse. Tu es un la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical. Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle? Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes. Si tu n'as pas les réponses a la question si la personne est consciente ET la réponse a la question si la personne respire tu as INTERDICTION de passer à la suite, tu dois redemander à l'utilisateur ces informations jusqu'a ce qu'il te réponde 'oui' ou 'non au minimus aux deux questions. Tu dois faire des phrases concises, tu es calme. UNIQUEMENT Si tu as obtenue la réponse a 1 et 2 et que l'utilisateur te demande quoi faire repond de cliquer sur le bouton et qu'un guide va suivre Tu DOIS connaitre la réponse a 1 et 2 avant de lui dire de cliquer. Tu DOIS dire de cliquer pour continuer UNIQUEMENT si la réponse a 1 ET 2 est negative"
+  SYSTEM_PROMPT =  <<-PROMPT
+  Oublies la politesse. Tu es la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical.
+
+  Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle?
+  Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes.
+  Tu dois faire des phrases concises, tu es calme.
+
+  UNIQUEMENT dans le cadre ou la personne n'est pas consciente et ne respire pas, Tu DOIS dire que c'est un arrêt cardiaque et inciter l'utilisateur a cliquer sur continuer pour acceder aux instructions.
+
+
+   UNIQUEMENT Si une des deux est positive, ce n'est pas un arrêt cardiaque, repond 'Appelez le 15 ils vont vous guider'
+  PROMPT
+
+
+
+
 
 
   def create
@@ -9,10 +24,21 @@ class MessagesController < ApplicationController
     @message.role = "user"
     @cases = Case.all
     if @message.save
+      user = @chat.messages.where(role: "user")
+      #premier message ejecté avec reject
+      user_messages = user.reject{|message| message.content == "aidez moi"}
+      if user_messages.first.downcase == "non"
+        #--> personne inconsciente --> declencher nouveau prompt
+        @ruby_llm_chat = RubyLLM.chat
+
+        #nouveau prompt oriente directement sur l'arret cardiaque en cas de non respiration
+      #else premier message.downcase == oui --> personne consciente --> déclencher nouveau prompt
+        #nouveau prompt oriente sur la surveillance de la respiration/autre probleme
+
       @ruby_llm_chat = RubyLLM.chat
       build_conversation_history
       prompt = "#{emmergency_case},#{SYSTEM_PROMPT}"
-      response = @ruby_llm_chat.with_instructions(prompt).ask(@message.content)
+      response = @ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat: @chat)
       redirect_to chat_path(@chat)
     else
@@ -31,6 +57,10 @@ class MessagesController < ApplicationController
       @ruby_llm_chat.add_message(message)
     end
   end
+
+
+
+
 
   def emmergency_case
     @choice = [] # Le but est que le chat puisse choisir la bonne urgence, PEUT ETRE faire un hash dans lequel on stock l'ID de l'urgence et plus tard on s'en sert pour afficher la show de la bonne case.
