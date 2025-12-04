352fbfa21584908264851d383102136d326c2a67
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-12-04 08:48:51 +0100

    code commenté

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,16 @@
 class MessagesController < ApplicationController
-  SYSTEM_PROMPT =  <<-PROMPT
-  Oublies la politesse. Tu es la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical.
+  # SYSTEM_PROMPT =  <<-PROMPT
+  # Oublies la politesse. Tu es la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical.
 
-  Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle?
-  Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes.
-  Tu dois faire des phrases concises, tu es calme.
+  # Tu dois obtenir la réponse a deux questions: 1 = La personne est elle consciente? 2 = La personne respire t'elle?
+  # Lorsque tu connais les réponses de 1 ET 2 , tu évoques clairement la situation dans laquelle nous sommes.
+  # Tu dois faire des phrases concises, tu es calme.
 
-  UNIQUEMENT dans le cadre ou la personne n'est pas consciente et ne respire pas, Tu DOIS dire que c'est un arrêt cardiaque et inciter l'utilisateur a cliquer sur continuer pour acceder aux instructions.
+  # UNIQUEMENT dans le cadre ou la personne n'est pas consciente et ne respire pas, Tu DOIS dire que c'est un arrêt cardiaque et inciter l'utilisateur a cliquer sur continuer pour acceder aux instructions.
 
 
-   UNIQUEMENT Si une des deux est positive, ce n'est pas un arrêt cardiaque, repond 'Appelez le 15 ils vont vous guider'
-  PROMPT
+  #  UNIQUEMENT Si une des deux est positive, ce n'est pas un arrêt cardiaque, repond 'Appelez le 15 ils vont vous guider'
+  # PROMPT
 
   PROMPT_NO_CONSCIENS = <<-PROMPT
   Oublies la politesse. Tu es la pour orienter sur une décision. Quelqu'un vient d'avoir un problème médical.
@@ -18,8 +18,7 @@ class MessagesController < ApplicationController
   Tu dois IMPERATIVEMENT demander 'Est ce qu'elle respire?'
   Tu as l'interdiction de répondre à l'utilisateur tant qu'il ne t'as pas répondu oui ou non.
 
-  Si la personne respire: cool
-
+  Si l'utilisateur repond oui: tu dois IMPERATIVEMENT repondre 'appelez les secours et mettez la personne en PLS'
   Si la personne ne respire pas tu dois OBLIGATOIREMENT Repondre 'c'est un arrêt cardiaque. L'application vas vous guider pour les manoeuvre de secours, appelez ou faites appelez le 15'
   PROMPT
   PROMPT_AWAKE = <<-PROMPT
@@ -35,29 +34,28 @@ class MessagesController < ApplicationController
     @cases = Case.all
     if @message.save
       user = @chat.messages.where(role: "user")
-      #premier message ejecté avec reject
+      #premier message "j'ai besoin d'aide" (celui généré automatiquement)ejecté avec .reject
       user_messages = user.reject{|message| message.content == "j'ai besoin d'aide"}
       if user_messages.first.content.downcase == "non"
         #--> personne inconsciente --> declencher nouveau prompt
         @ruby_llm_chat = RubyLLM.chat
-        build_conversation_history
-        prompt = PROMPT_NO_CONSCIENS
-        #nouveau prompt oriente directement sur l'arret cardiaque en cas de non respiration
+        build_conversation_history #--> sinon on boucle sur la question
+
+        #nouveau prompt doit savaoir que la personne est inconsciente --> oriente directement sur l'arret cardiaque en cas de non respiration
         response = @ruby_llm_chat.with_instructions(PROMPT_NO_CONSCIENS).ask(@message.content)
         Message.create(role: "assistant", content: response.content, chat: @chat)
-        # redirect_to chat_path(@chat)
+
 
       else  #premier message.downcase == oui --> personne consciente --> déclencher nouveau prompt
-        #nouveau prompt oriente sur la surveillance de la respiration/autre probleme
+        #nouveau prompt oriente sur la surveillance de la respiration --> on part sur un autre probleme
         @ruby_llm_chat = RubyLLM.chat
         build_conversation_history
-        prompt = PROMPT_NO_CONSCIENS
-        #nouveau prompt oriente directement sur l'arret cardiaque en cas de non respiration
+        #nouveau prompt oriente  sur l'appel des secours nous ne sommes plus dans le cadre de l'arret cardio respi
         response = @ruby_llm_chat.with_instructions(PROMPT_AWAKE).ask(@message.content)
         Message.create(role: "assistant", content: response.content, chat: @chat)
-        #redirect_to chat_path(@chat)
-      end
 
+      end
+        #########code de initial sans essayer de fractionner le prompt##############################
       # @ruby_llm_chat = RubyLLM.chat
       # build_conversation_history
       # prompt = "#{emmergency_case},#{SYSTEM_PROMPT}"
@@ -84,7 +82,7 @@ class MessagesController < ApplicationController
 
 
 
-
+######################## methode qui n'est actuellement pas utilisée, l'idee etait de donner la liste des eventuelle cases au LLM et qu'il choisisse la bonne....
   def emmergency_case
     @choice = [] # Le but est que le chat puisse choisir la bonne urgence, PEUT ETRE faire un hash dans lequel on stock l'ID de l'urgence et plus tard on s'en sert pour afficher la show de la bonne case.
     @cases.each do |emergency|
