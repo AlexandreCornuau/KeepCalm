a0e560b5efe4179b240690feda0ee66f80296b2e
camille hemet <camille.hemet@hotmail.fr>
2025-12-05 13:26:33 +0100

    design modal in progress

--- app/javascript/controllers/modal_urgence_controller.js
+++ app/javascript/controllers/modal_urgence_controller.js
@@ -3,13 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["btn", "modal", "close"]
 
-  connect() {
-    this.element.addEventListener("click", (event) => {
-      if (event.target == this.modalTarget) {
-        this.modalTarget.style.display = "none";
-      }
-    })
-  }
+  // connect() {
+  //   this.element.addEventListener("click", (event) => {
+  //     if (event.target == this.modalTarget) {
+  //       this.modalTarget.style.display = "none";
+  //     }
+  //   })
+  // }
 
   openModal(event) {
     if (event.target == this.btnTarget) {
