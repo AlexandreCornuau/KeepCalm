1ba5e713381c23ce721b1c94aff9c403595422ca
camille hemet <camille.hemet@hotmail.fr>
2025-12-08 16:34:58 +0100

    adding turbo frame

--- app/javascript/controllers/modal_urgence_controller.js
+++ app/javascript/controllers/modal_urgence_controller.js
@@ -8,16 +8,18 @@ export default class extends Controller {
   }
 
   connect() {
-    this.popModal()
+    // this.popModal()
   }
 
 
   // ouverture automatique de la modal
-  popModal(event) {
-    if (this.responseValue === "non" && this.counterValue >= 3) {
-      setTimeout(()=>{this.modalTarget.style.display = "block";}, 10);
-    }
-  }
+  // popModal(event) {
+  //   if (this.responseValue === "non" && this.counterValue >= 3) {
+  //     console.log("pop");
+  //     this.modalTarget.style.display = "block";
+  //   }
+  // }
+
   // fermer la modal
   closeModal(event) {
     this.modalTarget.style.display = "none";
