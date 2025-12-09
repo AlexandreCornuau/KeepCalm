d396da2284b56c0f5e163a77b6eba39162966e74
Quentin Luylier <qluylier@outlook.fr>
2025-12-09 17:03:15 +0100

    Fix Chrome configuration for Heroku

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -3,6 +3,7 @@ Grover.configure do |config|
   config.options = {
     format: 'A4',
     margin: { top: '1cm', bottom: '1cm', left: '1cm', right: '1cm' },
-    launch_args: ['--no-sandbox', '--disable-setuid-sandbox']
+    launch_args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+    executable_path: ENV['GOOGLE_CHROME_BIN'] || ENV['CHROME_BIN']
   }
 end
