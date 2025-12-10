696a1a299f0761119d618b39275dfe47a0ac1687
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-12-10 14:18:26 +0100

    puppetter

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,12 +1,13 @@
+# config/initializers/grover.rb
 Grover.configure do |config|
   config.options = {
     launchOptions: {
-      executablePath: ENV[‘PUPPETEER_EXECUTABLE_PATH’],
       args: %w[
         --no-sandbox
         --disable-setuid-sandbox
         --disable-gpu
         --disable-dev-shm-usage
+        --headless=new
       ]
     }
   }
