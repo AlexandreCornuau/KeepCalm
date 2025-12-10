26cac6edcb8a2488b6195c7bfda2eb8c596ec784
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2025-12-10 13:01:47 +0100

    grover

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,13 +1,13 @@
 Grover.configure do |config|
   config.options = {
-    format: 'A4',
-    margin: { top: '1cm', bottom: '1cm', left: '1cm', right: '1cm' },
-    launch_args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu'
-    ],
-    executable_path: ENV.fetch('CHROME_BIN', nil)
+    launchOptions: {
+      executablePath: ENV[‘PUPPETEER_EXECUTABLE_PATH’],
+      args: %w[
+        --no-sandbox
+        --disable-setuid-sandbox
+        --disable-gpu
+        --disable-dev-shm-usage
+      ]
+    }
   }
 end
